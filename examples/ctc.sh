4895d27a4c03c97908c2bfc6771691e4db755d7c
Rod Vagg <rod@vagg.org>
2016-07-26 10:45:03 -0700

    add ctc and tsc examples

---
+++ examples/ctc.sh
@@ -0,0 +1,37 @@
+GROUP_NAME="Core Technical Committee (CTC)"
+MEETING_TIME="Wednesday 8pm"
+INVITEES="\
+* Anna Henningsen @addaleax (observer)
+* Bradley Meck @bmeck (observer/GoDaddy/TC39)
+* Ben Noordhuis @bnoordhuis (CTC)
+* Сковорода Никита Андреевич @ChALkeR (CTC)
+* Chris Dickinson @chrisdickinson (CTC)
+* Colin Ihrig @cjihrig (CTC)
+* Evan Lucas @evanlucas (CTC)
+* Jeremiah Senkpiel @Fishrock123 (CTC)
+* Fedor Indutny @indutny (CTC)
+* James M Snell @jasnell (CTC)
+* Josh Gavant @joshgav (observer/Microsoft)
+* Michael Dawson @mhdawson (CTC)
+* Myles Borins @TheAlphaNerd (observer)
+* Julien Gilli @misterdjules (CTC)
+* Mikeal Rogers @mikeal (observer/Node.js Foundation)
+* Brian White @mscdex (CTC)
+* Ali Ijaz Sheikh @ofrobots (CTC)
+* Alexis Campailla @orangemocha (CTC)
+* Bert Belder @piscisaureus (CTC)
+* Rod Vagg @rvagg (CTC)
+* Seth Thompson @s3ththompson (observer/Google)
+* Shigeki Ohtsu @shigeki (CTC)
+* Steven R Loomis @srl295 (observer/IBM/ICU)
+* Trevor Norris @trevnorris (CTC)
+* Rich Trott @Trott (CTC)
+"
+JOINING_INSTRUCTIONS="Uberconference; participants should have the link & numbers, contact me if you don't.
+
+## Public participation
+
+We stream our conference call straight to YouTube so anyone can listen to it live, it should start playing at **https://www.youtube.com/c/nodejs+foundation/live** when we turn it on. There's usually a short cat-herding time at the start of the meeting and then occasionally we have some quick private business to attend to before we can start recording & streaming. So be patient and it should show up.
+
+Many of us will be on IRC in #node-dev on Freenode if you'd like to interact, we have a Q/A session scheduled at the end of the meeting if you'd like us to discuss anything in particular. @nodejs/collaborators in particular if there's anything you need from the CTC that's not worth putting on as a separate agenda item, this is a good place for it.
+"
