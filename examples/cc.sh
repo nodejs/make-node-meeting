18aef03cdad476d12f6e3b8d2d8d8ec7372cb8de
Tierney Cyren <accounts@bnb.im>
2017-12-14 10:55:17 -0500

    Updates to CommComm example
    Updates membership list for CommComm and modernizes Joining Instructions.

--- examples/cc.sh
+++ examples/cc.sh
@@ -1,27 +1,43 @@
 GROUP_NAME="Community Committee (CommComm)"
 MEETING_TIME="Thursday 8:30pm"
 INVITEES="
-* Ashley Williams @ashleygwilliams (CommComm)
-* Emily Rose @emilyrose (CommComm)
-* Gregor Martynus @gr2m (CommComm)
-* Joe Sepi @joesepi (observer)
-* JP Wesselink @jpwesselink (CommComm)
-* Michael Dawson @mhdawson (CommComm)
-* Michelle Garrett @msmichellegar (CommComm)
-* Myles Borins @MylesBorins (CommComm)
-* Rachel White @rachelnicole (CommComm)
-* Refael Ackermann @refack (observer)
-* Rod Vagg @rvagg (CommComm)
-* Tierney Cyren @bnb (CommComm)
-* Tracy Hinds @hackygolucky (CommComm)
-* William Kapke @williamkapke (CommComm)
-* Oz Haven @therebelrobot (observer)
+* @ashleygwilliams (Ashley Williams - CommComm)
+* @emilyrose (Emily Rose - CommComm)
+* @gr2m (Gregor Martynus - CommComm)
+* @joesepi (Joe Sepi - CommComm)
+* @jpwesselink (JP Wesselink - CommComm)
+* @mhdawson (Michael Dawson - CommComm)
+* @msmichellegar (Michelle Garrett - CommComm)
+* @rachelnicole (Rachel White - CommComm)
+* @bnb (Tierney Cyren - CommComm)
+* @hackygolucky (Tracy Hinds - CommComm)
+* @williamkapke (William Kapke - CommComm)
+* @refack (Refael Ackermann - CommComm)
+* @therebelrobot (Oz Haven - observer)
+* Manil (@chowdhurian - observer)
+* @ParidelPooya (Pooya Paridel - observer)
+* @rykerrumsey (Ryker Rumsey - observer)
+* @baez (Behzad Karim - observer)
+* @BinarySo1o (Therese Stirling - observer)
+* @JemBijoux (Jem Bezooyen - observer)
+* @amiller-gh (Adam Miller - observer)
+* @obensource (Ben Michel - observer)
+* @yosuke-furukawa (Yosuke Furukawa - observer)
+* @Maurice-Hayward (Maurice Hayward - observer)
+* @dshaw (Dan Shaw - observer)
+* @mikehostetler (Mike Hostetler - observer)
+* @sarahkconway (Sarah Conway - observer)
+* @Tiriel (Benjamin Zaslavsky - observer)
+* @DiegoZoracKy (Diego ZoracKy - observer)
+* @ZibbyKeaton (Zibby Keaton - observer)
+* @tobyfarley (Toby Farley - observer)
+* @Bamieh (Ahmad Bamieh - observer)
 "
-JOINING_INSTRUCTIONS="Google Hangouts; link will be posted here shortly before the meeting starts.
+JOINING_INSTRUCTIONS="Zoom Meeting; a link will be posted here shortly before the meeting starts.
 
 A standing invitation exists for all @nodejs/TSC members who are not also CommComm members to attend in observer capacity.
 
 ## Public participation
 
-We stream our conference call straight to YouTube so anyone can listen to it live, it should start playing at **https://www.youtube.com/c/nodejs+foundation/live** when we turn it on. There's usually a short cat-herding time at the start of the meeting and then occasionally we have some quick private business to attend to before we can start recording & streaming. So be patient and it should show up.
+We stream our conference call straight to YouTube so anyone can listen to it live, it should start playing at **https://www.youtube.com/channel/UCQPYJluYC_sn_Qz_XE-YbTQ/live** when we turn it on. There's usually a short cat-herding time at the start of the meeting and then occasionally we have some quick private business to attend to before we can start recording & streaming. So be patient and it should show up.
 "
