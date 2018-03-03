1d5e2ef3bdebc7479faed4ff4815a2f4c4803aa9
Chowdhurian <manil.chowdhury@gmail.com>
2018-03-02 19:26:24 -0800

    add Website Redesign Initiative example

---
+++ examples/wr.sh
@@ -0,0 +1,30 @@
+GROUP_NAME="Website Redesign Initiative"
+MEETING_TIME="Thursday 3:00pm"
+INVITEES="
+* @oe - Olivia Hugger
+* @fhemberger - Frederic Hemberger
+* @bnb - Tierney Cyren
+* @timothyis - Timothy
+* @jestho - Jesper Thøgersen
+* @JonahMoses - Jonah Moses
+* @amiller-gh - Adam Miller
+* @skllcrn - Christopher Skillicorn
+* @pierreneter - Pierre Neter
+* @emilypmendez - Emily Mendez
+* @darcyclarke - Darcy Clarke
+* @maddhruv - Dhruv Jain
+* @obensource - Ben Michel
+* @Fishrock123 - Jeremiah Senkpiel
+* @chowdhurian - Manil Chowdhury
+* @franciscop - Francisco Presencia
+"
+JOINING_INSTRUCTIONS="Zoom Meeting; a link will be posted here in the comments shortly before the meeting starts.
+
+If you're interested in joining the meeting, just open an issue in this repository to request an invite.
+
+## Public participation
+
+We stream our conference call straight to YouTube. Anyone can listen to it live. When we turn it on, it should start playing at https://www.youtube.com/channel/UCQPYJluYC_sn_Qz_XE-YbTQ/live. There's a short cat-herding time at the beginning of the meeting and, occasionally, we attend to some quick private business before we can start recording & streaming. Please be patient and the live stream should show up!
+
+Welcome to the Website Redesign Initiative! 💛
+"
